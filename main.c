eb096504d3f4b35f5cb9b06f09bc4f78c14eb02f
Carlo 'zED' Caputo <carlo.caputo@gmail.com>
2011-11-19 13:56:36 -0200

    rotozoom

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <SDL_image.h>
 #include <SDL_rotozoom.h>
 #include <stdio.h>
+#include <math.h>
 
 #define FPS 30
 #define MIN(a,b) ((a)<(b)?(a):(b))
@@ -24,16 +25,34 @@ Uint32 bmask = 0x00ff0000;
 Uint32 amask = 0xff000000;
 #endif
 
+typedef struct { int x,y; } point;
+typedef struct { float x,y; } vec;
+
+typedef struct {
+	point p;
+	vec v;
+	float angle;
+	float health;
+	int frame;
+};
 
 int main( int argc, char* args[] )
 {
     SDL_Surface* screen = NULL;
     SDL_Surface* sprite = NULL;
     SDL_Surface* icon = NULL;
+	SDL_Surface *player_frame = NULL;
 
 	// init
+	int sprite_width = 130;
+	int sprite_height = 130;
+	int sprite_bx = 520+10/*center*/;
+	int sprite_by = 650-16/*center*/;
+
 	SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO );
 	sprite = IMG_Load_RW( SDL_RWFromMem(sprite_jpg, sprite_jpg_len), 1 );
+	player_frame = SDL_CreateRGBSurface(SDL_SWSURFACE, sprite_width, sprite_height, 32,
+				rmask, gmask, bmask, amask);
 
 	// window manager
 	{
@@ -52,10 +71,11 @@ int main( int argc, char* args[] )
 
 	// player setup
 	int x=0, y=0;
-	int frame=0;
+	int frame=0, step=0;
 	int speed=8;
 	int up=0,down=0,left=0,right=0;
 	float accel=.2;
+	float angle=0;
 
 	// main loop
 	int running = 1;
@@ -90,21 +110,37 @@ int main( int argc, char* args[] )
 		left = left*(1-accel)+pressed[SDLK_LEFT ]*speed*accel;
 		right=right*(1-accel)+pressed[SDLK_RIGHT]*speed*accel;
 		//printf("x %d y %d up %d dw %d le %d ri %d acc %f\n",x,y,up,down,left,right,accel);
-		x+=right-left;
-		y+=down-up;
+		int dx=right-left;
+		int dy=down-up;
+		x+=dx;
+		y+=dy;
+		if(dx||dy) angle = atan2(-dy,dx);
+		//printf("%d %d %f\n",dx,dy, angle);
+
 		// TODO collision
 
-		if(right-left||down-up)
-			frame=(frame+1)%5;
+		// animation
+		if(dx||dy) {
+			step++;
+			if(step%4==0)
+				frame=(frame+((rand()%20)==0?2:1))%5;
+		}
 
 		// clean screen
 		SDL_FillRect(screen,NULL, 0);
 
 		// draw player
-		//SDL_FillRect(screen,&dst, 0xffffff);
-		SDL_Rect src = {(5+frame)*130,0,130,115};
 		SDL_Rect dst = {x,y,0,0};
-		SDL_BlitSurface( sprite, &src, screen, &dst );
+#if 0
+		SDL_FillRect(screen,&dst, 0xffffff);
+#else
+		SDL_Rect src = {sprite_bx+frame*sprite_width,sprite_by,sprite_width,sprite_height};
+		SDL_BlitSurface( sprite, &src, player_frame, NULL );
+		SDL_Surface *player_rotozoom = rotozoomSurface(player_frame, (angle)*180/M_PI, 1, SMOOTHING_ON);
+		SDL_BlitSurface( player_rotozoom, NULL, screen, &dst );
+		SDL_FreeSurface(player_rotozoom);
+#endif
+
 		SDL_Flip( screen );
 
 		// timing control
