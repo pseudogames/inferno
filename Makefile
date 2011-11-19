c84a0d5d4dd86e28d18eb1d2bc5f2b47ce0152ed
Carlo 'zED' Caputo <carlo.caputo@gmail.com>
2011-11-19 12:12:31 -0200

    build do sprite no codigo, icone e titulo na app e controle de framerate

--- Makefile
+++ Makefile
@@ -1,11 +1,21 @@
-PROGRAM=inferno
 
-
-all: $(PROGRAM)
+all: inferno
 
 clean:
-	rm -v $(PROGRAM)
+	rm -fv inferno inferno.exe sprite.jpg sprite.c main.o sprite.o
 
-$(PROGRAM): main.c
-	gcc $^ `sdl-config --cflags --libs` -o $@
-	
+sprite.jpg: ~/Dropbox/inferno/zombie_topdown.png 
+	convert $< $@
+
+sprite.c: sprite.jpg
+	xxd -i $^ > $@
+
+sprite.o: sprite.c
+	gcc $< -c -o $@
+
+main.o: main.c
+	gcc $< `sdl-config --cflags` -c -o $@
+
+inferno: main.o sprite.o
+	gcc $^ -lSDL_image `sdl-config --libs` -o $@
+
--- main.c
+++ main.c
@@ -1,16 +1,50 @@
 #include "SDL.h"
-#include "stdio.h"
+#include "SDL_image.h"
+
+#define FPS 30
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+
+extern unsigned char sprite_jpg[];
+extern unsigned int sprite_jpg_len;
+
+/* SDL interprets each pixel as a 32-bit number, so our masks must depend
+   on the endianness (byte order) of the machine */
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+Uint32 rmask = 0xff000000;
+Uint32 gmask = 0x00ff0000;
+Uint32 bmask = 0x0000ff00;
+Uint32 amask = 0x000000ff;
+#else
+Uint32 rmask = 0x000000ff;
+Uint32 gmask = 0x0000ff00;
+Uint32 bmask = 0x00ff0000;
+Uint32 amask = 0xff000000;
+#endif
+
 
 int main( int argc, char* args[] )
 {
-    SDL_Surface* sprite = NULL;
     SDL_Surface* screen = NULL;
+    SDL_Surface* sprite = NULL;
+    SDL_Surface* icon = NULL;
 
 	SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO );
 
-	screen = SDL_SetVideoMode( 1024, 768, 32, SDL_SWSURFACE );
+	sprite = IMG_Load_RW( SDL_RWFromMem(sprite_jpg, sprite_jpg_len), 1 );
 
-	sprite = SDL_LoadBMP( "sprite.bmp" );
+	{
+		icon = SDL_CreateRGBSurface(SDL_SWSURFACE, 64, 64, 32,
+				rmask, gmask, bmask, amask);
+
+		SDL_Rect src = {32,32,64,64};
+		SDL_Rect dst = {0,0,0,0};
+		SDL_BlitSurface( sprite, &src, icon, &dst );
+		SDL_WM_SetIcon(icon, NULL);
+	}
+	SDL_WM_SetCaption("inferno", "inferno");
+
+	screen = SDL_SetVideoMode( 1024, 768, 32, SDL_SWSURFACE );
 
 	int dx=0, dy=0;
 	int x=0, y=0;
@@ -18,6 +52,7 @@ int main( int argc, char* args[] )
 	int d=4;
 
 	int running = 1;
+	unsigned int t=0,lt=0;
 	while(running) {
 		SDL_Event event;
 		if( SDL_PollEvent( &event ) )
@@ -52,10 +87,15 @@ int main( int argc, char* args[] )
 		SDL_BlitSurface( sprite, &src, screen, &dst );
 		//SDL_FillRect(screen,&dst, 0xffffff);
 		SDL_Flip( screen );
-		SDL_Delay(100);
+		lt = t; t = SDL_GetTicks();
+		int actual_delta = t-lt;
+		int expected_delta = 1000/FPS;
+		int delay = MAX(0, expected_delta - actual_delta);
+		SDL_Delay(delay);
 	}
 
     SDL_FreeSurface( sprite );
+    //SDL_FreeSurface( icon );
 
     SDL_Quit();
 
